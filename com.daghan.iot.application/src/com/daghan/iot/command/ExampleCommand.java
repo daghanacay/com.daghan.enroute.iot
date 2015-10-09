075e537a6306341b05b1fb676aff5f7f43e8729d
daghan <daghanacay@gmail.com>
2015-10-09 20:26:19 +1100

    Adding the configuration for circuit application

--- com.daghan.iot.application/src/com/daghan/iot/command/ExampleCommand.java
+++ com.daghan.iot.application/src/com/daghan/iot/command/ExampleCommand.java
@@ -1,14 +1,42 @@
 package com.daghan.iot.command;
 
+import java.io.IOException;
+
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
 
-@Component
-public class ExampleCommand {
+import osgi.enroute.debug.api.Debug;
+import osgi.enroute.iot.gpio.api.CircuitBoard;
+import osgi.enroute.iot.gpio.api.IC;
+import osgi.enroute.iot.gpio.util.Digital;
+import osgi.enroute.iot.gpio.util.ICAdapter;
 
+
+
+@Component(
+		service = IC.class, 
+		
+		property = {
+			Debug.COMMAND_SCOPE+"=domo", 
+			Debug.COMMAND_FUNCTION+"=led"
+		}
+	)
+public class ExampleCommand extends ICAdapter<Void, Digital> {
+
+	public void led(boolean on) throws Exception {
+		out().set(on);
+		System.out.println("LED is set to " + on);
+	}
+
+	@Reference
+	protected void setCircuitBoard(CircuitBoard cb) {
+		super.setCircuitBoard(cb);
+	}
+	
 	@Activate
-	void activate() {
+	void activate() throws IOException, InterruptedException {
 		System.out.println("Hello World");
 	}
 

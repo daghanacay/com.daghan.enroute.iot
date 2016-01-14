afcfd6f2dd235871681290b6d2a7d355029b9d08
daghan <daghanacay@gmail.com>
2016-01-14 21:09:30 +1100

    Creating the Config form

--- com.daghan.resource.application/web/configModule.js
+++ com.daghan.resource.application/web/configModule.js
@@ -7,29 +7,44 @@ function($http, $filter) {
     this.configs = [];
     this.selectedConfig = null;
     this.childFrom = null;
-    this.showTheForm = true;
-    this.selectConfig = function(config){
+    this.showTheForm = false;
+    this.selectConfig = function(config) {
 	this.selectedConfig = config;
 	this.showTheForm = false;
     };
-	
-	// Create a child form
-    this.createChildForm = function(config) {
-	this.childForm = {
-	    title : config.pidString,
-	    description : "testDescription"
-	};
+
+    // Create a child form for a new configuration
+    this.newChildForm = function(config) {
+	$http({
+	    method : 'GET',
+	    url : '/system/console/configMgr/'+config.pidString,
+	    params:{'post':'true'}
+	}).then(function successCallback(response) {
+	    vm.childForm = response.data;
+	}, function errorCallback(response) {
+	    this.alerts.push({
+		type : 'failure',
+		msg : response
+	    });
+	});
 	this.showTheForm = true;
 	this.selectedConfig = config;
     };
-   
+
+    // Create an update form for existing child
+    this.updateChildForm = function(config) {
+	// TODO
+	this.showTheForm = true;
+	this.selectedConfig = config;
+    };
+
     // Get the bundles
     $http({
 	method : 'GET',
 	url : '/rest/configurations'
     }).then(function successCallback(response) {
 	vm.configs = response.data;
-	vm.selectedConfig = vm.configs[0];
+	vm.selectConfig(vm.configs[0]);
     }, function errorCallback(response) {
 	this.alerts.push({
 	    type : 'failure',

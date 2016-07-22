a4592fdfef913e77efb34e4101c40f37ffabc8c0
daghan <daghanacay@gmail.com>
2016-07-23 09:13:12 +1000

    adding the heat map application

---
+++ com.daghan.heatmap.application/src/com/daghan/heatmap/application/HeatmapApplication.java
@@ -0,0 +1,23 @@
+package com.daghan.heatmap.application;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+import com.daghan.iot.resource.manager.ResourceRunner;
+
+import osgi.enroute.configurer.api.RequireConfigurerExtender;
+import osgi.enroute.google.angular.capabilities.RequireAngularWebResource;
+import osgi.enroute.rest.api.REST;
+import osgi.enroute.twitter.bootstrap.capabilities.RequireBootstrapWebResource;
+import osgi.enroute.webserver.capabilities.RequireWebServerExtender;
+
+@RequireAngularWebResource(resource = { "angular.js", "angular-resource.js", "angular-route.js" }, priority = 1000)
+@RequireBootstrapWebResource(resource = "css/bootstrap.css")
+@RequireWebServerExtender
+@RequireConfigurerExtender
+@Component(name = "com.daghan.heatmap")
+public class HeatmapApplication implements REST {
+	@Reference
+	private ResourceRunner rm;
+
+}
